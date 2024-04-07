0ad3d27b78c2c63d5315293cd352fc50e451329e
dabamascodes <daniel.6@live.com.mx>
2024-04-07 16:36:32 -0600

    http requests

--- http.js
+++ http.js
@@ -1,15 +1,41 @@
 // Vamos a crear un web server
 
-const http = require('http');
+// const http = require('http');
 
-// http.createServer(function (request, response) {
+// // http.createServer(function (request, response) {
+// //     response.write('Hello world');
+// //     response.end();
+// // }).listen(3000);
+
+// http.createServer((request, response) => {
 //     response.write('Hello world');
 //     response.end();
 // }).listen(3000);
 
-http.createServer((request, response) => {
-    response.write('Hello world');
-    response.end();
-}).listen(3000);
+// console.log('Servidor escuchando en el puerto 3000');
 
-console.log('Servidor escuchando en el puerto 3000');
+const http = require("http");
+
+const server = http.createServer((request, response) => {
+  console.log(request.url);
+
+  if (request.url === "/") {
+    response.write("Welcome to the server");
+    return response.end();
+  }
+
+  if (request.url === "/about") {
+    response.write("acerca de");
+    return response.end();
+  }
+
+  response.write(`
+    <h1>Not Found</h1>
+    <p>esta pagina no se encontro<p>
+    <a href="/">Volver a la pagina principal</a>
+    `);
+  response.end();
+});
+
+server.listen(3000);
+console.log("Servidor escuchando en el puerto 3000");
