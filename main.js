0cc75bfb63410f7bcb9f381e1119ca2f20876270
dabamascodes <daniel.6@live.com.mx>
2024-04-07 13:56:28 -0600

    Primer modulo

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 // const web = require('./module/myModule');
 // console.log(web);
 
-const { myArray, myNumber, myWebAddress, user } = require('./module/myModule');
+// const { myArray, myNumber, myWebAddress, user } = require('./module/myModule');
+// console.log(myArray);
+// console.log(myNumber);
+// console.log(myWebAddress);
+// console.log(user);
 
-console.log(myArray);
-console.log(myNumber);
-console.log(myWebAddress);
-console.log(user);
+const math = require('./math/index');
+console.log(math.add(10,20));
+console.log(math.substract(10,20));
+console.log(math.multiply(10,20));
+console.log(math.divide(10,20));
