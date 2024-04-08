10ae877de4e3ba55f4ef9879e29d6af409a5822a
dabamascodes <daniel.6@live.com.mx>
2024-04-08 16:14:57 -0600

    fs promises

--- promise.js
+++ promise.js
@@ -36,35 +36,57 @@
 // read();
 
 /////////////////////////////////////////////////////////////////////////
-// Promisify
+// // Promisify
 
-// `util.promisify()` in Node.js converts callback-based methods to promise-based, aiding in managing asynchronous code more cleanly. This alleviates callback nesting issues, enhancing code readability, and simplifying asynchronous operations through promise chaining.
-const { readFile } = require('fs');
-const { promisify } = require('util');
+// // `util.promisify()` in Node.js converts callback-based methods to promise-based, aiding in managing asynchronous code more cleanly. This alleviates callback nesting issues, enhancing code readability, and simplifying asynchronous operations through promise chaining.
+// const { readFile } = require('fs');
+// const { promisify } = require('util');
 
-// const getText = (pathFile) => {
-//     return new Promise(function (resolve, reject) {
-//         readFile(pathFile, 'utf-8', (err, data) => {
-//             if (err) {
-//                 reject(err);
-//             };
-//             resolve(data);
-//         });
-//     });
+// // const getText = (pathFile) => {
+// //     return new Promise(function (resolve, reject) {
+// //         readFile(pathFile, 'utf-8', (err, data) => {
+// //             if (err) {
+// //                 reject(err);
+// //             };
+// //             resolve(data);
+// //         });
+// //     });
+// // };
+// // Promisify -----> getText
+
+// const getText = promisify(readFile);
+
+// async function read() {
+//     try {
+//         const result = await getText('./data/first.txt', 'utf-8');
+//         console.log(result);
+//         const result2 = await getText('./data/second.txt', 'utf-8');
+//         console.log(result2);
+//         const result3 = await getText('./data/third.txt', 'utf-8');
+//         console.log(result3);
+//         const result4 = await getText('./data/fourth.txt', 'utf-8');
+//         console.log(result4);
+//     } catch (error) {
+//         console.log(error);
+//     };
 // };
-// Promisify -----> getText
 
-const getText = promisify(readFile);
+// read();
+
+/////////////////////////////////////////////////////////////////////////
+// Fn Promises
+// fs/promises ----> es una funcion tipo Promesa para ReadFiles
+const { readFile } = require('fs/promises');
 
 async function read() {
     try {
-        const result = await getText('./data/first.txt', 'utf-8');
+        const result = await readFile('./data/first.txt', 'utf-8');
         console.log(result);
-        const result2 = await getText('./data/second.txt', 'utf-8');
+        const result2 = await readFile('./data/second.txt', 'utf-8');
         console.log(result2);
-        const result3 = await getText('./data/third.txt', 'utf-8');
+        const result3 = await readFile('./data/third.txt', 'utf-8');
         console.log(result3);
-        const result4 = await getText('./data/fourth.txt', 'utf-8');
+        const result4 = await readFile('./data/fourth.txt', 'utf-8');
         console.log(result4);
     } catch (error) {
         console.log(error);
