f71e115ff4194faafdbf348ddfa99cf7cccbd112
dabamascodes <daniel.6@live.com.mx>
2024-04-08 16:02:53 -0600

    Promisify

--- promise.js
+++ promise.js
@@ -1,21 +1,62 @@
+// const { readFile } = require('fs');
+
+// const getText = (pathFile) => {
+//     return new Promise(function (resolve, reject) {
+//         readFile(pathFile, 'utf-8', (err, data) => {
+//             if (err) {
+//                 reject(err);
+//             };
+//             resolve(data);
+//         });
+//     });
+// };
+
+// // getText('./data/fourth.txt')
+// // .then((result) => console.log(result))
+// // .then(() => getText('./data/first.txt'))
+// // .then((result) => console.log(result))
+// // .catch(error => console.log(error));
+
+// async function read() {
+//     try {
+//         const result = await getText('./data/first.txt');
+//         console.log(result);
+//         const result2 = await getText('./data/second.txt');
+//         console.log(result2);
+//         throw new Error("Esto es un Error creado para testear el Try Catch");
+//         const result3 = await getText('./data/third.txt');
+//         console.log(result3);
+//         const result4 = await getText('./data/fourth.txt');
+//         console.log(result4);
+//     } catch (error) {
+//         console.log(error);
+//     };
+// };
+
+// read();
+
+/////////////////////////////////////////////////////////////////////////
+// Promisify
+
+// `util.promisify()` in Node.js converts callback-based methods to promise-based, aiding in managing asynchronous code more cleanly. This alleviates callback nesting issues, enhancing code readability, and simplifying asynchronous operations through promise chaining.
 const { readFile } = require('fs');
+const { promisify } = require('util');
 
-const getText = (pathFile) => {
-    return new Promise(function (resolve, reject) {
-        readFile(pathFile, 'utf-8', (err, data) => {
-            if (err) {
-                reject(err);
-            };
-            resolve(data);
-        });
-    });
-};
+// const getText = (pathFile) => {
+//     return new Promise(function (resolve, reject) {
+//         readFile(pathFile, 'utf-8', (err, data) => {
+//             if (err) {
+//                 reject(err);
+//             };
+//             resolve(data);
+//         });
+//     });
+// };
+// Promisify -----> getText
 
-// getText('./data/fourth.txt')
-// .then((result) => console.log(result))
-// .then(() => getText('./data/first.txt'))
-// .then((result) => console.log(result))
-// .catch(error => console.log(error));
+
+
+const getText = promisify(readFile);
 
 async function read() {
     try {
@@ -23,7 +64,6 @@ async function read() {
         console.log(result);
         const result2 = await getText('./data/second.txt');
         console.log(result2);
-        throw new Error("Esto es un Error creado para testear el Try Catch");
         const result3 = await getText('./data/third.txt');
         console.log(result3);
         const result4 = await getText('./data/fourth.txt');
@@ -34,7 +74,3 @@ async function read() {
 };
 
 read();
-
-
-
-
